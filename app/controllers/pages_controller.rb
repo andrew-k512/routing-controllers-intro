79c20b128357a66a7f8110c712f27584b739cb5f
andrew-k512 <andrew.fiveonetwo@gmail.com>
2017-07-19 15:46:45 -0400

    save h1 to @header instance variable for each page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,17 @@
 class PagesController < ApplicationController
 
   def welcome
+    @header = "Awesome Welcome Page"
     render :welcome
   end
 
   def about
+   @header = "About Us"
    render :about
   end
 
   def contest
+    @header = "Contest Info"
     render :contest
   end
 
